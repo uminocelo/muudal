c0537da9a4d7c122b913bb8192adaaf9b046e76b
uminocelo <marcelo.mdl.dev@gmail.com>
2023-07-01 00:27:22 -0300

    update home_live.ex

--- lib/muudal_web/page/home_live.ex
+++ lib/muudal_web/page/home_live.ex
@@ -15,9 +15,14 @@ defmodule MuudalWeb.Page.HomeLive do
   def render(assigns) do
     ~H"""
     <h1 class="font-semibold text-3xl mb-4">Cows</h1>
-    <Muudalweb.CoreComponents.button>
+
+    <MuudalWeb.CoreComponents.modal id="create_modal">
+      <h2>Add a pet.</h2>
+    </MuudalWeb.CoreComponents.modal>
+
+    <MuudalWeb.CoreComponents.button phx-click={MuudalWeb.CoreComponents.show_modal("create_modal")}>
       Add New Cow +
-    </Muudalweb.CoreComponents.button>
+    </MuudalWeb.CoreComponents.button>
     """
   end
 
